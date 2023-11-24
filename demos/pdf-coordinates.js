184e69da987f6e7713571a35bb70ca4a022b706c
warjiang <1096409085@qq.com>
2023-11-24 18:04:15 +0800

    feat: add hs demo

--- demos/pdf-coordinates.js
+++ demos/pdf-coordinates.js
@@ -2,15 +2,48 @@ import { getDocument } from "pdfjs-dist/legacy/build/pdf.mjs";
 
 const rootPath = '/workspaces/gpt4-pdf-chatbot-langchain'
 const pdfPath = `${rootPath}/docs/raft.pdf`;
-
+const pageNum = 1;
 
 
 (async () => {
     const doc = await getDocument(pdfPath).promise;
-    const numPages = doc.numPages;
-    console.log("# Document Loaded");
-    console.log("Number of Pages: " + numPages);
-    console.log();
+    const page = await doc.getPage(pageNum);
+    const pageWidth = page.view[2];
+    const pageHeight = page.view[3];
+    console.log('pageWidth', pageWidth, 'pageHeight', pageHeight);
+
+
+    const viewport = page.getViewport();
+    const content = await page.getTextContent();
+    const hs = content.items.map((item) => {
+        // console.log(item)
+        /*
+        let x = item.transform[4] / pageWidth;
+        let y = item.transform[5] / pageHeight;
+        let width = item.width / pageWidth;
+        let height = item.height / pageHeight;
+        */
+       let x1 = item.transform[4] ;
+    //    let y1 = item.transform[5];
+       let y1 = (pageHeight - item.transform[5]) - item.height ;
+       let x2 = (item.transform[4] + item.width) ;
+       let y2 = y1 + item.height;
+       let width = pageWidth ;
+       let height = pageHeight // 注意 y1 > y2
+    //    console.log(viewport.convertToViewportRectangle(x1, y1, x2, y2))
+        return {
+            content: {
+                text: item.str,
+            },
+            position: {
+                boundingRect: { x1, y1, x2, y2, width, height, pageNumber: pageNum },
+                rects: [{ x1, y1, x2, y2, width, height, pageNumber: pageNum }],
+            },
+            comment: 'xxxx',
+        }
+    })
+    console.log(JSON.stringify(hs[4],null, 4))
+    // console.log(JSON.stringify(hs, null, 4))
     /*
     const loadingTask = getDocument(pdfPath);
     const doc = await loadingTask.promise;
